fb42be70950ed89d58630463067ec837aba98719
澤なつみ <n2natsu1218@gmail.com>
2018-02-02 20:46:43 +0900

    refacta

--- tsunk-brain/app/controllers/ScrapingController.scala
+++ tsunk-brain/app/controllers/ScrapingController.scala
@@ -15,22 +15,18 @@ class  ScrapingController @Inject() extends Controller {
 
   def catchWords = Action { implicit request =>
     val parentPageDoc = JsoupBrowser().get("https://www.uta-net.com/search/?Aselect=3&Bselect=3&Keyword=%E3%81%A4%E3%82%93%E3%81%8F&sort=&pnum=1")
-    val urlList = for {
+    val result = for {
+      // 親ページから200曲分の歌詞ページURLを取得
       area <- parentPageDoc >> elements("table > tbody > tr > td")
       url = area >> (attrs("href")("a"))
-    } yield {
-      url.filter(url => url != Nil).filter(url => url.startsWith("/song"))
-    }
-
-    val wordsResult = for {
-      url <- urlList
-      songPageUrl <- url
+      songPageUrl <- url.filter(url => url != Nil).filter(url => url.startsWith("/song"))
+      // 1ページずつ見に行き歌詞を取得
       doc = JsoupBrowser().get("https://www.uta-net.com" + songPageUrl)
       area <- doc >> elements("#flash_area")
       words = area >> text("#kashi_area")
     } yield words
 
-    println(wordsResult)
+    println(result)
 
     Ok(views.html.index())
   }
